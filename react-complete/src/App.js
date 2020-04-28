91dcf44dc08d5c23e85e33ff86925cfe9d620ea2
Satya Kumar Itekela <st798799@dal.ca>
2020-04-27 21:38:18 -0300

    error handling using component

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 const app = props => {
   const [personsState, updatePersonsState] = useState({
