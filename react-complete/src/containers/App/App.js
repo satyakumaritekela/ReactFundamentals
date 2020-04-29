2e962a941231f4490ccc0342d37ced95f2c76ea5
Satya Kumar Itekela <st798799@dal.ca>
2020-04-28 21:56:41 -0300

    updating app.js

--- react-complete/src/containers/App/App.js
+++ react-complete/src/containers/App/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Persons from '../../components/Persons/Persons';
 import Cockpit from '../../components/Cockpit/Cockpit';
 import Radium, { StyleRoot } from '../../../node_modules/radium';
 
-const App = props => {
-  const [personsState, updatePersonsState] = useState({
+class App extends React.Component {
+  state = {
     persons: [
       {name: 'Satya', age: 24},
       {name: 'Max', age: 25},
@@ -13,69 +13,70 @@ const App = props => {
     ],
     willShow: false,
     buttonValue: "Show Persons"
-  });
+  };
 
-  const buttonHandler = () => {
-    personsState.willShow ? updatePersonsState({
+  buttonHandler = () => {
+    this.state.willShow ? this.setState({
       buttonValue: "Show Persons",
-      willShow: !personsState.willShow
-    }) : updatePersonsState({
+      willShow: !this.state.willShow
+    }) : this.setState({
       persons: [
         {name: 'Satya', age: 24},
         {name: 'Max', age: 25},
         {name: 'Sumo', age: 28},
       ],
       buttonValue: "Hide Persons",
-      willShow: !personsState.willShow
+      willShow: !this.state.willShow
     });  
   }
 
-  const deleteHandler = (personIndex) => {
-    let persons = [...personsState.persons];
+  deleteHandler = (personIndex) => {
+    let persons = [...this.state.persons];
     persons.splice(personIndex, 1)
-    updatePersonsState({
+    this.setState({
       persons: persons,
-      buttonValue: personsState.buttonValue,
-      willShow: personsState.willShow
+      buttonValue: this.state.buttonValue,
+      willShow: this.state.willShow
     })
   }
 
-  const changeHandler = (event, personIndex) => {
-    let persons = [...personsState.persons];
+  changeHandler = (event, personIndex) => {
+    let persons = [...this.state.persons];
 
-    let person = { ...personsState.persons[personIndex] }
+    let person = { ...this.state.persons[personIndex] }
     person.name = event.target.value;
 
     persons[personIndex] = person;
 
-    updatePersonsState({
+    this.setState({
       persons: persons,
-      buttonValue: personsState.buttonValue,
-      willShow: personsState.willShow
+      buttonValue: this.state.buttonValue,
+      willShow: this.state.willShow
     })
   }
+  render() {
+    let showPersons = null;
 
-  let showPersons = null;
+    if (this.state.willShow) {
+      showPersons =  <Persons
+            persons={this.state.persons}
+            change={this.changeHandler}
+            delete={this.deleteHandler} />
+    }
 
-  if (personsState.willShow) {
-    showPersons =  <Persons
-          persons={personsState.persons}
-          change={changeHandler}
-          delete={deleteHandler} />
+    return (
+      <StyleRoot>
+        <div className="App">
+          <Cockpit
+            willShow={this.state.willShow}
+            buttonhandler={this.buttonHandler}
+            buttonvalue={this.state.buttonValue}
+          />
+          {showPersons}
+        </div>
+      </StyleRoot>
+    );
   }
-
-  return (
-    <StyleRoot>
-      <div className="App">
-        <Cockpit
-          willShow={personsState.willShow}
-          buttonhandler={buttonHandler}
-          buttonvalue={personsState.buttonValue}
-        />
-        {showPersons}
-      </div>
-    </StyleRoot>
-  );
 }
 
 export default Radium(App);
